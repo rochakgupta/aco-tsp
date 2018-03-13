5b5ac01c6e62f593b53752e54f3b662e78576fc2
Rochak Gupta <rochak84@gmail.com>
2018-03-14 01:42:17 +0530

    Multiple changes
    -Remove parameters 'num_nodes', 'x_range' and 'y_range' from aco_tsp.py.
    -Add parameter 'labels' to aco_tsp.py.

--- aco_tsp.py
+++ aco_tsp.py
@@ -50,8 +50,7 @@ class SolveTSPUsingACO:
             return self.distance
 
     def __init__(self, mode='ACS', colony_size=10, elitist_weight=1.0, min_scaling_factor=0.001, alpha=1.0, beta=3.0,
-                 rho=0.1, pheromone_deposit_weight=1.0, initial_pheromone=1.0, steps=100, num_nodes=20,
-                 x_range=(0, 500), y_range=(0, 500), nodes=None):
+                 rho=0.1, pheromone_deposit_weight=1.0, initial_pheromone=1.0, steps=100, nodes=None, labels=None):
         self.mode = mode
         self.colony_size = colony_size
         self.elitist_weight = elitist_weight
@@ -59,15 +58,12 @@ class SolveTSPUsingACO:
         self.rho = rho
         self.pheromone_deposit_weight = pheromone_deposit_weight
         self.steps = steps
-        if nodes is not None:
-            self.num_nodes = len(nodes)
-            self.nodes = nodes
+        self.num_nodes = len(nodes)
+        self.nodes = nodes
+        if labels is not None:
+            self.labels = labels
         else:
-            self.num_nodes = num_nodes
-            self.nodes = [
-                (random.uniform(x_range[0], x_range[1]), random.uniform(y_range[0], y_range[1]))
-                for _ in range(0, self.num_nodes)
-            ]
+            self.labels = range(1, self.num_nodes + 1)
         self.edges = [[None] * self.num_nodes for _ in range(self.num_nodes)]
         for i in range(self.num_nodes):
             for j in range(i + 1, self.num_nodes):
@@ -134,18 +130,16 @@ class SolveTSPUsingACO:
                         self.edges[i][j].pheromone = min_pheromone
 
     def run(self):
-        print('Started: {0}'.format(self.mode))
+        print('Started : {0}'.format(self.mode))
         if self.mode == 'ACS':
             self._acs()
         elif self.mode == 'Elitist':
             self._elitist()
         else:
             self._max_min()
-        print('Ended: {0}'.format(self.mode))
-        print('Tour: [{0}]'.format(
-            ' -> '.join([' -> '.join(str(i + 1) for i in self.global_best_tour), str(self.global_best_tour[0] + 1)])
-        ))
-        print('Distance: {0}\n'.format(round(self.global_best_distance, 2)))
+        print('Ended : {0}'.format(self.mode))
+        print('Sequence : <- {0} ->'.format(' - '.join(str(self.labels[i]) for i in self.global_best_tour)))
+        print('Total distance travelled to complete the tour : {0}\n'.format(round(self.global_best_distance, 2)))
 
     def plot(self, line_width=1, point_radius=math.sqrt(2.0), annotation_size=8, dpi=120, save=True, name=None):
         x = [self.nodes[i][0] for i in self.global_best_tour]
@@ -156,7 +150,7 @@ class SolveTSPUsingACO:
         plt.scatter(x, y, s=math.pi * (point_radius ** 2.0))
         plt.title(self.mode)
         for i in self.global_best_tour:
-            plt.annotate(i + 1, self.nodes[i], size=annotation_size)
+            plt.annotate(self.labels[i], self.nodes[i], size=annotation_size)
         if save:
             if name is None:
                 name = '{0}_tour.png'.format(self.mode)
@@ -166,12 +160,15 @@ class SolveTSPUsingACO:
 
 
 if __name__ == '__main__':
-    acs = SolveTSPUsingACO(mode='ACS', colony_size=5, steps=50, num_nodes=15, x_range=(-400, 400), y_range=(-400, 400))
+    _colony_size = 5
+    _steps = 50
+    _nodes = [(random.uniform(-400, 400), random.uniform(-400, 400)) for _ in range(0, 15)]
+    acs = SolveTSPUsingACO(mode='ACS', colony_size=_colony_size, steps=_steps, nodes=_nodes)
     acs.run()
     acs.plot()
-    elitist = SolveTSPUsingACO(mode='Elitist', colony_size=5, steps=50, nodes=acs.nodes)
+    elitist = SolveTSPUsingACO(mode='Elitist', colony_size=_colony_size, steps=_steps, nodes=_nodes)
     elitist.run()
     elitist.plot()
-    max_min = SolveTSPUsingACO(mode='MaxMin', colony_size=5, steps=50, nodes=acs.nodes)
+    max_min = SolveTSPUsingACO(mode='MaxMin', colony_size=_colony_size, steps=_steps, nodes=_nodes)
     max_min.run()
     max_min.plot()
